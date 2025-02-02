56f25a07f00b11f0dc5514d4f6e156cff4da523e
svisnu@gmail.com <svisnusai@gmail.com>
2025-02-02 08:49:02 +0530

    re intiating

---
+++ doc_a.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+Shell
+Navigation to local repos to remote repos codefile transfer
+> Create a github repos
+> Install IDE work space
+> Git clone to local repos
+> create a code file in work space
+> Add code file to staging area
+> Commit the code file
+> Export the code file
+
+Syntax to add code file to local repos
+cd /c/repos/
+git clone (link)
+cd decentral
+git add (file name)
+git config --global user.email "email"
+git config --global user.name "your name"
+git commit -m "type text"
+
+Syntax to add code file from local repos to remote repos
+Note: open cresential manager and remove github credentials if need to connect freshly.
+git ph origin main
+(login to github using tocken)
+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
+git log
+
+git rm --cached (file name)
